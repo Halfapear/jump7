77ffd71de420f9d6a67208f84acf74698304d865
MartinJZB <89769396+MartinJZB@users.noreply.github.com>
2023-08-13 08:05:29 +0800

    Add files via upload

---
+++ jump7.py
@@ -0,0 +1,9 @@
+for number in range(1, 101):
+    if (number == 7 or number == 14 or number == 17 or number == 21 or number == 27 or
+        number == 28 or number == 35 or number == 37 or number == 42 or number == 47 or
+        number == 49 or number == 56 or number == 63 or number == 70 or number == 77 or
+        number == 84 or number == 91 or number == 98 or '7' in str(number)):
+
+        continue
+    else:
+	print(number)
