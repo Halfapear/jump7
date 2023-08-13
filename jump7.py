427bae79b75d6e6f5a7317aea3064dd421155f91
MartinJZB <89769396+MartinJZB@users.noreply.github.com>
2023-08-13 08:08:21 +0800

    Update jump7.py

--- jump7.py
+++ jump7.py
@@ -2,7 +2,9 @@ for number in range(1, 101):
     if (number == 7 or number == 14 or number == 17 or number == 21 or number == 27 or
         number == 28 or number == 35 or number == 37 or number == 42 or number == 47 or
         number == 49 or number == 56 or number == 63 or number == 70 or number == 77 or
-        number == 84 or number == 91 or number == 98 or '7' in str(number)):
+        number == 84 or number == 91 or number == 98 or number == 57 or number == 67 or 
+        number == 87 or number == 97 or number == 71 or number == 72 or number == 73 or 
+	number == 74 or number == 75 or number == 76 or number == 78 or number == 79):
 
         continue
     else:
